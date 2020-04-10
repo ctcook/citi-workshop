b36ebc8056068c7fe261021593417af4de6ec8de
Chuck <ccook4@g.clemson.edu>
2020-04-10 11:01:31 -0400

    Update install_webserver.sh

--- install_webserver.sh
+++ install_webserver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
-sudo apt-get update
-sudo apt-get install -y apache2
+sudo apt update
+sudo apt install -y apache2
 sudo ufw allow in "Apache Full"
 sudo systemctl status apache2
